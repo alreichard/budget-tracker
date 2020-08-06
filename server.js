32539020732f2c17e36e79998c76b35c0e0e7106
alreicha <areichard@haverford.edu>
2020-08-06 16:08:38 -0400

    mongo db

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+require("dotenv").config()
 
 const PORT = 3000;
 
@@ -15,11 +16,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
-
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/mern",
+  {
+    useMongoClient: true
+  }
+);
 // routes
 app.use(require("./routes/api.js"));
 
